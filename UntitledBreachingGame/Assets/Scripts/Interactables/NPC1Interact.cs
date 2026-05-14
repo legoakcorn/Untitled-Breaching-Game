094093a08507fc9cf562c68376f012b2dfaa2cd5
legoakcorn <declanrmurray@icloud.com>
2026-05-14 08:25:07 -0400

    lobbyNPCUpdate

--- UntitledBreachingGame/Assets/Scripts/Interactables/NPC1Interact.cs
+++ UntitledBreachingGame/Assets/Scripts/Interactables/NPC1Interact.cs
@@ -1,13 +1,20 @@
+using System.Runtime.CompilerServices;
+using System.Threading;
 using UnityEngine;
 
 public class NPC1Interact : NewInteractables
 {
     [SerializeField] private GameObject NPCText;
+    [SerializeField] private float NPCTextDuration;
     protected override void Interact()
     {
         NPCText.SetActive(true);
         InputMovement.Freeze = true;
-        
+        Invoke(nameof(ResetText), NPCTextDuration);
+    }
+
+    private void ResetText()
+    {
         NPCText.SetActive(false);
         InputMovement.Freeze = false;
     }
